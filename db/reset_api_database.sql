881cf9ad67193b7bc14ced31141bc3468d17cfcf
mrz1836 <mrz1818@gmail.com>
2019-08-17 20:24:28 -0400

    Added basic db schema for goose

---
+++ db/reset_api_database.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS api_example;
+CREATE DATABASE api_example;
+DROP USER IF EXISTS 'apiDbTestUser'@'%';
+CREATE USER 'apiDbTestUser'@'%' IDENTIFIED BY 'ThisIsSecureEnough123';
+GRANT USAGE ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT DELETE ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT INSERT ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT SELECT ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT UPDATE ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT CREATE ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT INDEX ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT ALTER ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT REFERENCES ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT DROP ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT CREATE TEMPORARY TABLES ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT TRIGGER ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT SUPER ON *.* TO 'apiDbTestUser'@'%';
+FLUSH PRIVILEGES;
