5651c389117c7d6550186518fd48fba7745ce970
mrz1836 <mrz1818@gmail.com>
2019-08-19 13:52:30 -0400

    Customized for SQL boiler

--- db/reset_api_database.sql
+++ db/reset_api_database.sql
@@ -1,6 +1,13 @@
+# Drop the current database
 DROP DATABASE IF EXISTS api_example;
+
+# Build a new database
 CREATE DATABASE api_example;
+
+# Drop any existing user
 DROP USER IF EXISTS 'apiDbTestUser'@'%';
+
+# Create the user and assign grants
 CREATE USER 'apiDbTestUser'@'%' IDENTIFIED BY 'ThisIsSecureEnough123';
 GRANT USAGE ON `api_example`.* TO 'apiDbTestUser'@'%';
 GRANT DELETE ON `api_example`.* TO 'apiDbTestUser'@'%';
@@ -12,7 +19,11 @@ GRANT INDEX ON `api_example`.* TO 'apiDbTestUser'@'%';
 GRANT ALTER ON `api_example`.* TO 'apiDbTestUser'@'%';
 GRANT REFERENCES ON `api_example`.* TO 'apiDbTestUser'@'%';
 GRANT DROP ON `api_example`.* TO 'apiDbTestUser'@'%';
+GRANT LOCK TABLES ON `api_example`.* TO 'apiDbTestUser'@'%';
 GRANT CREATE TEMPORARY TABLES ON `api_example`.* TO 'apiDbTestUser'@'%';
 GRANT TRIGGER ON `api_example`.* TO 'apiDbTestUser'@'%';
 GRANT SUPER ON *.* TO 'apiDbTestUser'@'%';
 FLUSH PRIVILEGES;
+
+# Database was generated from SQLBoiler
+GRANT ALL ON `fqgmsvujyikggnnkpkfbldubvmtcirjhohkivzex`.* to 'apiDbTestUser'@'%';
