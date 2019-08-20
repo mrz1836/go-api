b258dd92ce777ef124b52169aecbda6c60d1f4b1
mrz1836 <mrz1818@gmail.com>
2019-08-19 20:46:43 -0400

    Better comment

--- setup_db.sh
+++ setup_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Runs locally to reset your database and seed the new database
+# Runs locally to reset your database and seed the api example database schema
 mysql -u root < ./db/reset_api_database.sql && goose -dir "db/migrations" mysql "apiDbTestUser:ThisIsSecureEnough123@/api_example?parseTime=true" up
