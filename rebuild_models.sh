a24c887493f9c16c89c8556f382d7a6a384370a4
mrz1836 <mrz1818@gmail.com>
2019-08-19 20:46:55 -0400

    Fixed to work in the new location

--- rebuild_models.sh
+++ rebuild_models.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sqlboiler mysql --wipe && sed -i "" 's/fmt.Fprintf(tmp, "ssl-mode/\/\/fmt.Fprintf(tmp, "ssl-mode/' models/mysql_main_test.go
+# Wipes out the folder, replace the ssl-mode (fix for MariaDB)
+sqlboiler mysql --wipe && sed -i "" 's/fmt.Fprintf(tmp, "ssl-mode/\/\/fmt.Fprintf(tmp, "ssl-mode/' models/generated/mysql_main_test.go
