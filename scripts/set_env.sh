9ffc347fa88d2a0babbd1dcdae2acd426d6ef0bc
mrz1836 <mrz1818@gmail.com>
2019-08-23 13:20:36 -0400

    New env format

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -9,29 +9,17 @@ export API_ENVIRONMENT=development
 #
 # Read Database
 #
-export API_DATABASE_DRIVER=mysql
-export API_DATABASE_MAX_CONN_LIFETIME=60
-export API_DATABASE_READ_HOST=locahost
-export API_DATABASE_READ_MAX_IDLE_CONNECTIONS=5
-export API_DATABASE_READ_MAX_OPEN_CONNECTIONS=5
-export API_DATABASE_READ_NAME=api_example
-export API_DATABASE_READ_PORT=3306
-export API_DATABASE_READ_USER=apiDbTestUser
-export API_DATABASE_READ_PASSWORD=ThisIsSecureEnough123
+export API_DATABASE_READ__HOST=localhost
+export API_DATABASE_READ__USER=apiDbTestUser
+export API_DATABASE_READ__PASSWORD=ThisIsSecureEnough123
 
 #
 # Write Database
 #
-export API_DATABASE_DRIVER=mysql
-export API_DATABASE_MAX_CONN_LIFETIME=60
-export API_DATABASE_WRITE_HOST=locahost
-export API_DATABASE_WRITE_MAX_IDLE_CONNECTIONS=5
-export API_DATABASE_WRITE_MAX_OPEN_CONNECTIONS=5
-export API_DATABASE_WRITE_NAME=api_example
-export API_DATABASE_WRITE_PORT=3306
-export API_DATABASE_WRITE_USER=apiDbTestUser
-export API_DATABASE_WRITE_PASSWORD=ThisIsSecureEnough123
+export API_DATABASE_WRITE__HOST=localhost
+export API_DATABASE_WRITE__USER=apiDbTestUser
+export API_DATABASE_WRITE__PASSWORD=ThisIsSecureEnough123
 
 # If you want to use cache, set the redis url
 # Example: redis://localhost:6379
-export API_CACHE_URL=your-cache-url
+export API_CACHE__URL='redis://localhost:6379'
