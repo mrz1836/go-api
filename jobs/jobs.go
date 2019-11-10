eda7b5e29132465e186e89f1e2f40c7172006866
mrz1836 <mrz1818@gmail.com>
2019-11-10 14:24:24 -0500

    Example jobs package with job

---
+++ jobs/jobs.go
@@ -0,0 +1,34 @@
+/*
+Package jobs is for all scheduled jobs (tasks) to run
+*/
+package jobs
+
+import (
+	"fmt"
+
+	"github.com/mrz1836/go-api/config"
+	"github.com/mrz1836/go-logger"
+)
+
+// exampleJob is an example job
+func exampleJob() {
+
+	logger.Data(2, logger.DEBUG, "starting job...")
+
+	// Do something
+
+	// Do something else
+
+	logger.Data(2, logger.DEBUG, "job complete!")
+}
+
+// RunExampleJob will run the job every X minutes
+func RunExampleJob(runNow bool, andEveryXMinutes int) {
+	if runNow {
+		exampleJob()
+	}
+	err := config.Values.Scheduler.AddJob("example-job", fmt.Sprintf("@every %dm", andEveryXMinutes), exampleJob)
+	if err != nil {
+		logger.Data(2, logger.ERROR, "error adding job: RunExampleJob: "+err.Error())
+	}
+}
